a6ff8d8855dafa59ca0f8520d0ee0d9895914d6a
suomiy <suomiy@gmail.com>
2020-10-20 21:12:09 +0200

    update cluster-test makefiles

--- automation/e2e-tests.sh
+++ automation/e2e-tests.sh
@@ -5,15 +5,7 @@ set -ex
 export SCOPE="${SCOPE:-cluster}"
 export STORAGE_CLASS="${STORAGE_CLASS:-}"
 export DEPLOY_NAMESPACE="${DEPLOY_NAMESPACE:-e2e-tests-$(shuf -i10000-99999 -n1)}"
-
 export IMAGE_REGISTRY_USER="$DEPLOY_NAMESPACE"
-
-if [[ "$SCOPE" == "cluster" ]]; then
-    export TEST_NAMESPACE="${TEST_NAMESPACE:-e2e-tests-$(shuf -i10000-99999 -n1)}"
-else
-    export TEST_NAMESPACE="${TEST_NAMESPACE:-$DEPLOY_NAMESPACE}"
-fi
-export TARGET_NAMESPACE="$TEST_NAMESPACE"
 
 make lint
 make test
@@ -21,7 +13,6 @@ make test-generated-tasks-consistency
 
 ./automation/e2e-deploy-resources.sh
 
-oc get namespaces -o name | grep -Eq "^namespace/$TEST_NAMESPACE$" || oc new-project "$TEST_NAMESPACE"
 oc get namespaces -o name | grep -Eq "^namespace/$DEPLOY_NAMESPACE$" || oc new-project "$DEPLOY_NAMESPACE"
 
 make cluster-sync
@@ -29,6 +20,4 @@ make cluster-sync
 # Wait for kubevirt to be available
 oc wait -n kubevirt kv kubevirt --for condition=Available --timeout 15m
 
-oc project "$DEPLOY_NAMESPACE"
-
-make e2e-tests-no-deploy
+make cluster-test
