098df72046b7378a17e3fac3e859dc2b7e5d5d83
suomiy <suomiy@gmail.com>
2021-01-21 12:31:00 +0100

    fixup scripts
    Signed-off-by: suomiy <suomiy@gmail.com>

--- scripts/common.sh
+++ scripts/common.sh
@@ -8,6 +8,15 @@ function leave() {
   popd > /dev/null
 }
 
+export IS_OPENSHIFT="false"
+export IS_MINIKUBE="false"
+
+if kubectl get projects > /dev/null 2>&1; then
+  export IS_OPENSHIFT="true"
+elif minikube status | grep -q Running; then
+  export IS_MINIKUBE="true"
+fi
+
 export EXCLUDED_NON_IMAGE_MODULES="shared|sharedtest|tests"
 export DEPENDENCY_MODULES="shared|sharedtest"
 
@@ -23,13 +32,3 @@ TASK_NAME_TO_ENV_NAME["execute-in-vm"]="EXECUTE_IN_VM_IMAGE"
 TASK_NAME_TO_IMAGE["execute-in-vm"]="${EXECUTE_IN_VM_IMAGE}"
 TASK_NAME_TO_ENV_NAME["cleanup-vm"]="EXECUTE_IN_VM_IMAGE"
 TASK_NAME_TO_IMAGE["cleanup-vm"]="${EXECUTE_IN_VM_IMAGE}"
-
-export IS_OPENSHIFT="false"
-export IS_MINIKUBE="false"
-
-if kubectl get projects > /dev/null 2>&1; then
-  export IS_OPENSHIFT="true"
-elif minikube status | grep -q Running; then
-  export IS_MINIKUBE="true"
-fi
-
