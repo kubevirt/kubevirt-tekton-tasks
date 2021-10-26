cfe3b16db03b5ebcc06d3f433d18fbf49abb53d6
Karel Šimon <ksimon@redhat.com>
2021-10-26 06:42:39 +0200

    pin ginkgo and gomega to 1.15 version
    1.16 versions are currently not working with our tests.
    
    Signed-off-by: Karel Šimon <ksimon@redhat.com>

--- automation/build-test.sh
+++ automation/build-test.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
+export GO111MODULE=on
 go get github.com/jstemmer/go-junit-report
 
-go get github.com/onsi/ginkgo/ginkgo
-go get github.com/onsi/gomega/...
-go get github.com/onsi/ginkgo/extensions/table
+go get github.com/onsi/ginkgo/ginkgo@v1.15.2
+go get github.com/onsi/gomega/...@v1.15.0
+go get github.com/onsi/ginkgo/extensions/table@v1.15.2
