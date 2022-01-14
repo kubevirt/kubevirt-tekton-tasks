d11d825ab1f14d6dc94a1fc3acd0b97d699b83e5
Karel Šimon <ksimon@redhat.com>
2022-01-14 11:44:13 +0100

    switch to libguestfs-tools image for disk-virt-customize and disk-virt-sysprep
    use quay.io/kubevirt/libguestfs-tools:v0.49.0 image as
    base image for for disk-virt-customize and disk-virt-sysprep.
    libguestfs-tools image contains all necessary libguestfs sw which is
    used by kubevirt.
    
    Signed-off-by: Karel Šimon <ksimon@redhat.com>

--- modules/disk-virt-sysprep/pkg/constants/constants.go
+++ modules/disk-virt-sysprep/pkg/constants/constants.go
@@ -10,6 +10,6 @@ const (
 
 const (
 	DiskImagePath               = "/mnt/targetpvc/disk.img"
-	GuestFSApplianceArchivePath = "/data/appliance.tar.xz"
+	GuestFSApplianceArchivePath = "/usr/local/lib/guestfs/downloaded"
 	VirtSysprepCommandsFileName = "virt_sysprep_commands"
 )
