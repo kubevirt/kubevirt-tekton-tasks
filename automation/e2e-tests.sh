161717d1f391bd8e670e2d45c7eeb49f15836b63
Karel Simon <ksimon@redhat.com>
2023-08-10 20:11:12 +0200

    fix: custom configuration of crio in testing cluster
    our e2e are not working due to permission issue in crio. cri-o/cri-o#7192 should revert the change.
    Until that revert PR is released and available in ci envs, we have to set special configuration on crio on all nodes
    
    Signed-off-by: Karel Simon <ksimon@redhat.com>

--- automation/e2e-tests.sh
+++ automation/e2e-tests.sh
@@ -8,7 +8,7 @@ export DEPLOY_NAMESPACE="${DEPLOY_NAMESPACE:-e2e-tests-$(shuf -i10000-99999 -n1)
 export NUM_NODES=${NUM_NODES:-2}
 
 # See scripts/common.sh for IMAGE env variable names
-
+./automation/set-crio-permissions-command.sh
 ./automation/e2e-deploy-resources.sh
 
 kubectl get namespaces -o name | grep -Eq "^namespace/$DEPLOY_NAMESPACE$" || kubectl create namespace "$DEPLOY_NAMESPACE"
