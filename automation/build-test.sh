cac3cd16d992389d8e62ab50116da6becbfa4ddb
Karel Simon <ksimon@redhat.com>
2024-04-04 12:42:28 +0200

    chore: bump ginkgo package
    add rule for dependabot to ignore ginkgo, gomega packages,
    because they need to be bumped manually.
    Bump ginkgo to newest version
    
    Signed-off-by: Karel Simon <ksimon@redhat.com>

--- automation/build-test.sh
+++ automation/build-test.sh
@@ -2,5 +2,5 @@
 
 GOFLAGS='' go install github.com/jstemmer/go-junit-report@latest
 
-GOFLAGS='' go install github.com/onsi/ginkgo/v2/ginkgo@v2.1.3
-GOFLAGS='' go install github.com/onsi/gomega/...@v1.19.0
+GOFLAGS='' go install github.com/onsi/ginkgo/v2/ginkgo@v2.17.1
+GOFLAGS='' go install github.com/onsi/gomega/...@v1.32.0
