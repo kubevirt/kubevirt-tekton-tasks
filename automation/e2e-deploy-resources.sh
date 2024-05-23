c0b7b1c43bdc710a20e4406584a94bc75eefa940
Karel Simon <ksimon@redhat.com>
2024-05-23 13:05:16 +0200

    chore: reenable ssp operator
    the ssp operator is required, because it imports linux images in special
    namespace which are then used by tests
    
    Signed-off-by: Karel Simon <ksimon@redhat.com>

--- automation/e2e-deploy-resources.sh
+++ automation/e2e-deploy-resources.sh
@@ -15,6 +15,9 @@ CDI_VERSION=$(curl -s https://api.github.com/repos/kubevirt/containerized-data-i
 TEKTON_VERSION=$(curl -s https://api.github.com/repos/tektoncd/operator/releases | \
             jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
 
+SSP_OPERATOR_VERSION=$(curl -s  https://api.github.com/repos/kubevirt/ssp-operator/releases | \
+            jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
+
 INSTANCE_TYPES_VERSION=$(curl -s  https://api.github.com/repos/kubevirt/common-instancetypes/releases | \
             jq '.[] | select(.prerelease==false) | .tag_name' | sort -V | tail -n1 | tr -d '"')
 
@@ -42,8 +45,27 @@ kubectl apply -f "https://github.com/kubevirt/containerized-data-importer/releas
 
 kubectl apply -f "https://github.com/kubevirt/containerized-data-importer/releases/download/${CDI_VERSION}/cdi-cr.yaml"
 
+# Deploy SSP
+kubectl apply -f "https://github.com/kubevirt/ssp-operator/releases/download/${SSP_OPERATOR_VERSION}/ssp-operator.yaml"
+
 # wait for tekton pipelines
 kubectl rollout status -n openshift-operators deployment/openshift-pipelines-operator --timeout 10m
+
+kubectl wait -n kubevirt deployment ssp-operator --for condition=Available --timeout 10m
+kubectl create -f - <<EOF
+apiVersion: ssp.kubevirt.io/v1beta2
+kind: SSP
+metadata:
+  name: ssp-sample
+  namespace: kubevirt
+spec:
+  featureGates:
+    deployCommonInstancetypes: false
+  commonTemplates:
+    namespace: openshift
+  templateValidator:
+    replicas: 1
+EOF
 
 # wait until tasks tekton CRD is properly deployed
 timeout 10m bash <<- EOF
