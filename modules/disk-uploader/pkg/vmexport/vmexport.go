d0999c6835525040193125d314c6b856f6b71a23
Ben Oukhanov <boukhanov@redhat.com>
2024-11-03 20:14:13 +0200

    feat(CNV-40411): set default instancetype and preference labels
    During execution of the disk-uploader, it should
    be able to get the default-instancetype and
    default-preference labels from the DV or PVC,
    and set them in the newly created containerDisk
    metadata afterwards.
    
    Signed-off-by: Ben Oukhanov <boukhanov@redhat.com>

--- modules/disk-uploader/pkg/vmexport/vmexport.go
+++ modules/disk-uploader/pkg/vmexport/vmexport.go
@@ -8,6 +8,7 @@ import (
 	"github.com/kubevirt/kubevirt-tekton-tasks/modules/shared/pkg/ownerreference"
 
 	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/util/wait"
 
@@ -88,6 +89,36 @@ func GetRawDiskUrlFromVolumes(client kubecli.KubevirtClient, namespace, name, vo
 		}
 	}
 	return "", fmt.Errorf("volume %s is not found in VirtualMachineExport internal volumes", volumeName)
+}
+
+func GetLabelsFromExportSource(virtClient kubecli.KubevirtClient, exportSourceKind, exportSourceNamespace, exportSourceName, volumeName string) (map[string]string, error) {
+	switch exportSourceKind {
+	case "VirtualMachine", "VirtualMachineSnapshot":
+		return getLabelsFromVirtualMachineOrSnapshot(virtClient, exportSourceNamespace, volumeName)
+	case "PersistentVolumeClaim":
+		return getLabelsFromPVC(virtClient, exportSourceNamespace, exportSourceName)
+	default:
+		return nil, fmt.Errorf("unsupported source kind: %s", exportSourceKind)
+	}
+}
+
+func getLabelsFromVirtualMachineOrSnapshot(virtClient kubecli.KubevirtClient, namespace, volumeName string) (map[string]string, error) {
+	dv, err := virtClient.CdiClient().CdiV1beta1().DataVolumes(namespace).Get(context.Background(), volumeName, metav1.GetOptions{})
+	if err == nil {
+		return dv.GetLabels(), nil
+	}
+	if !errors.IsNotFound(err) {
+		return nil, err
+	}
+	return getLabelsFromPVC(virtClient, namespace, volumeName)
+}
+
+func getLabelsFromPVC(virtClient kubecli.KubevirtClient, namespace, name string) (map[string]string, error) {
+	pvc, err := virtClient.CoreV1().PersistentVolumeClaims(namespace).Get(context.Background(), name, metav1.GetOptions{})
+	if err != nil {
+		return nil, err
+	}
+	return pvc.GetLabels(), nil
 }
 
 func getExportSource(exportSourceKind, exportSourceName string) (corev1.TypedLocalObjectReference, error) {
